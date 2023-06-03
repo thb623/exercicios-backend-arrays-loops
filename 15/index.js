8fba549944b253677d8a9fbc7e04b184911d555f
Thiago Bernardes <ailaikheda2000@gmail.com>
2023-06-03 07:27:59 -0300

    exercicio finalizado

--- 15/index.js
+++ 15/index.js
@@ -2,3 +2,12 @@ const original = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const pares = [];
 const impares = [];
 
+for(num of original){
+    if(num % 2 === 0){
+        pares.push(num)
+    }else{
+        impares.push(num)
+    }
+}
+console.log(pares)
+console.log(impares)
